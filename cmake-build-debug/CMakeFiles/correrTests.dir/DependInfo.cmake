248c375074ea78b9173a854986a5472ae329c88c
fboncini <fboncini@dc.uba.ar>
2018-04-07 19:09:07 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/correrTests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/fernando/Downloads/tp1_base/tp1_base/tests/test_instruccion.cpp" "/cygdrive/c/Users/fernando/Downloads/tp1_base/tp1_base/cmake-build-debug/CMakeFiles/correrTests.dir/tests/test_instruccion.cpp.o"
+  "/cygdrive/c/Users/fernando/Downloads/tp1_base/tp1_base/tests/test_programa.cpp" "/cygdrive/c/Users/fernando/Downloads/tp1_base/tp1_base/cmake-build-debug/CMakeFiles/correrTests.dir/tests/test_programa.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../tests/google-test/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/cygdrive/c/Users/fernando/Downloads/tp1_base/tp1_base/cmake-build-debug/tests/google-test/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/cygdrive/c/Users/fernando/Downloads/tp1_base/tp1_base/cmake-build-debug/tests/google-test/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrTests.dir/tests/test_instruccion.cpp.o"
+  "CMakeFiles/correrTests.dir/tests/test_programa.cpp.o"
+  "correrTests.pdb"
+  "correrTests.exe"
+  "libcorrerTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correrTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
